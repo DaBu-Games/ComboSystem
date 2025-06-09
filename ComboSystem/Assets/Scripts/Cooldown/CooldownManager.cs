794ba0af10896790a0396c3e89b8c9212517ea86
DaBu Games <buitelaardaan2004@gmail.com>
2025-06-09 11:55:39 +0200

    Added a cooldown manager that i can now check if the player can attack again or still in the old attack.

---
+++ ComboSystem/Assets/Scripts/Cooldown/CooldownManager.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class CooldownManager
+{
+    private float _lastUsedTime = 0f;
+    private float _cooldownDuration = 0f;
+    private bool _isOnCooldown = false;
+
+    public void StartCooldown(float duration)
+    {
+        _lastUsedTime = Time.deltaTime;
+        _cooldownDuration = duration;
+        _isOnCooldown = true;
+    }
+
+    public bool IsOnCooldown()
+    {
+        if (!_isOnCooldown)
+            return _isOnCooldown;
+
+        if (Time.time - _lastUsedTime >= _cooldownDuration)
+        {
+            _isOnCooldown = false;
+            return _isOnCooldown;
+        }
+        return true;
+    }
+}
