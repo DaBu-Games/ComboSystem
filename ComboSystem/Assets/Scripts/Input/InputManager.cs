e8744d4f248315acc40621025e08dd7c8a944db0
DaBu Games <buitelaardaan2004@gmail.com>
2025-06-09 11:43:06 +0200

    I added a input manager where you can add the diffrent input bindigs for every state the player has. Now if the state switches you can update the current bindings the player should use to attack. For example change the current bindings to air attacks when the player switches to the air state

---
+++ ComboSystem/Assets/Scripts/Input/InputManager.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager
+{
+    private readonly Dictionary<IState, Dictionary<AttackType, ICommand>> _inputBindings = new(); 
+    private Dictionary<AttackType, ICommand> _currentInputBindigs = new();
+    private IState _currentState;
+
+    public void AddInputBinding(IState state, AttackType attackType, ICommand command)
+    {
+        if (!_inputBindings.ContainsKey(state))
+        {
+            _inputBindings[state] = new Dictionary<AttackType, ICommand>(); 
+        }
+        
+        _inputBindings[state][attackType] = command;
+    }
+
+    public void ChangeState(IState state)
+    {
+        _currentState = state;
+        ChangeCurrentInputBinding(); 
+    }
+
+    private void ChangeCurrentInputBinding()
+    {
+        _currentInputBindigs.Clear();
+
+        if (_currentState != null && _inputBindings.TryGetValue(_currentState, out Dictionary<AttackType, ICommand> bindings))
+        {
+            _currentInputBindigs = bindings;
+        }
+    }
+}
