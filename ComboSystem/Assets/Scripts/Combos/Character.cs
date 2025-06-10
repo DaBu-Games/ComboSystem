f72bca86d396dc762265497e75865c84bae9220f
DaBu Games <buitelaardaan2004@gmail.com>
2025-06-10 13:23:00 +0200

    Change the attack class to a scripptable object. now i can make the attack more easily. I also changed the attackhistory class to now work with the scriptable object attack.

--- ComboSystem/Assets/Scripts/Combos/Character.cs
+++ ComboSystem/Assets/Scripts/Combos/Character.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "NewCombinations", menuName = "Combat/Combinations")]
+[CreateAssetMenu(fileName = "NewCharacter", menuName = "Combat/Character")]
 public class Combinations : ScriptableObject
 {
     [SerializeField] private List<Combination> combinations = new List<Combination>();
